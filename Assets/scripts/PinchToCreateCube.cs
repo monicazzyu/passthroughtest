b65a0d7a1d238de6ede73e7e9eab00ff54774aa4
zoeyyyyyyyyyyy <67435499+zoeyyyyyyyyyyy@users.noreply.github.com>
2023-03-13 17:30:29 -0700

    pinch 2.0

--- Assets/scripts/PinchToCreateCube.cs
+++ Assets/scripts/PinchToCreateCube.cs
@@ -3,16 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Oculus;
 
-[RequireComponent(typeof(Collider))]
+
 
 public class PinchToCreateCube : MonoBehaviour
 {
     private OVRHand[] m_hands;
-    private Vector3 pinchPosition;
-    private GameObject newObject;
-    public Transform indexFingerTip;
+    private float x1, y1, z1;
+
+    public GameObject ballPrefab;
+    public GameObject IndexBall;
+    public int maxBalls = 3;
+    
+    private List<GameObject> balls = new List<GameObject>();
+    private int IndexBallMoveCount = 0;
+    private Vector3 prevIndexBallPos;
+
     
     
 
@@ -31,11 +37,51 @@ public class PinchToCreateCube : MonoBehaviour
     void Update()
     {
         if (m_hands[0].GetFingerIsPinching(OVRHand.HandFinger.Index))
-            indexFingerTip = GameObject.Find("hands:b_l_index_ignore").transform;
-            GameObject newObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            newObject.transform.position = indexFingerTip.position;
+        {
+            if (IndexBall.transform.position != prevIndexBallPos)
+            {
+                IndexBallMoveCount++;
+                prevIndexBallPos = IndexBall.transform.position;
+
+                // If IndexBall moved less than 4 times, create new ball
+                if (IndexBallMoveCount < 4)
+                {
+                    CreateBall(prevIndexBallPos);
+                }
+                else // Otherwise, delete all balls and create new one
+                {
+                    DestroyBalls();
+                    CreateBall(prevIndexBallPos);
+                    IndexBallMoveCount = 0;
+                }
+            }
+        }
+
+
+
         
 
             
+    }
+
+    void CreateBall(Vector3 pos)
+    {
+        if (balls.Count >= maxBalls)
+        {
+            Destroy(balls[0]); // Delete oldest ball
+            balls.RemoveAt(0);
+        }
+
+        GameObject ball = Instantiate(ballPrefab, pos, Quaternion.identity);
+        balls.Add(ball);
+    }
+
+    void DestroyBalls()
+    {
+        foreach (GameObject ball in balls)
+        {
+            Destroy(ball);
+        }
+        balls.Clear();
     }
 }
